f9fbd1cc17a7dd16c517efb7efa3063bf62b2091
gaby magnussen <gabymagnussen@gmail.com>
2020-03-02 16:00:57 -0600

    adding JS

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-
 const inquirer = require("inquirer");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+.prompt([
+            {
+                name: 'name',
+                message: 'What is your name?',
+                type: 'input',
+            },
+            {
+                name:'email',
+                message: 'What is your email?',
+                type: 'input',
+            },
+            {
+                name: 'ID',
+                message: 'What is your ID?',
+                type: 'input',
+            },
+            {
+                name: 'Position',
+                message: 'What is your position?',
+                type: 'input',
+            }
+        
+           
+        ])
+        .then(answers => {
+            console.log(answers);
+            let file = `
+            # Title
+            Project Title ${answers.projectTitle}
+            `
+            fs.writeFile('README.md', file, () => {})
+        });
